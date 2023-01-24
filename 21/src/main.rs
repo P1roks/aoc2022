6263e917640f5086dd70feacd5c79aedf2c0c2fa
P1roks <piotrekjakobczyk1@gmail.com>
2023-01-24 13:12:52 +0100

    day 21 part 2

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::ops as o;
-
-#[derive(Debug)]
+use std::time::Instant;
+#[derive(Debug, Clone)]
 enum Oper {
     Add,
     Sub,
@@ -16,29 +16,66 @@ impl Oper {
             + o::Div<U, Output = F>
             + o::Mul<U, Output = F>,
     {
+        use Oper::*;
         match self {
-            Self::Add => item_1 + item_2,
-            Self::Sub => item_1 - item_2,
-            Self::Div => item_1 / item_2,
-            Self::Mul => item_1 * item_2,
+            Add => item_1 + item_2,
+            Sub => item_1 - item_2,
+            Div => item_1 / item_2,
+            Mul => item_1 * item_2,
+        }
+    }
+    //gotta love math
+    fn do_oper_right<F, U>(&self, item_1: F, item_2: U) -> F
+    where
+        F: o::Sub<U, Output = F> + o::Div<U, Output = F>,
+        U: o::Sub<F, Output = F> + o::Div<F, Output = F>,
+    {
+        use Oper::*;
+        match self {
+            Add => item_2 - item_1,
+            Sub => item_1 - item_2,
+            Div => item_1 / item_2,
+            Mul => item_2 / item_1,
         }
     }
     fn from(og: &str) -> Self {
+        use Oper::*;
         match og {
-            "+" => Self::Add,
-            "-" => Self::Sub,
-            "/" => Self::Div,
-            "*" => Self::Mul,
+            "+" => Add,
+            "-" => Sub,
+            "/" => Div,
+            "*" => Mul,
             _ => panic!("Non supported operation!"),
+        }
+    }
+    fn opposite(&self) -> Self {
+        use Oper::*;
+        match self {
+            Add => Sub,
+            Sub => Add,
+            Mul => Div,
+            Div => Mul,
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Monkey {
     first_monkey: String,
     second_monkey: String,
     oper: Oper,
+}
+
+//djb2, bcs imagine using Strings as keys
+fn hash(val: &str) -> String {
+    /* let mut hash: u64 = 5381;
+
+    for c in val.as_bytes() {
+        hash = ((hash << 5) + hash) + *c as u64;
+    } */
+
+    // hash
+    val.to_owned()
 }
 
 fn parse_input(file: &str) -> (HashMap<String, Monkey>, HashMap<String, u64>) {
@@ -48,17 +85,17 @@ fn parse_input(file: &str) -> (HashMap<String, Monkey>, HashMap<String, u64>) {
 
     for line in file.lines() {
         let mut mnk = line.split(": ");
-        let name = mnk.next().unwrap().to_string();
+        let name = hash(mnk.next().unwrap());
 
         //job of a given monkey
         let job = mnk.next().unwrap();
-        if let Ok(val) = job.parse::<u64>() {
+        if let Ok(val) = job.parse::<_>() {
             us_monkeys.insert(name, val);
         } else {
             let mut val = job.split(' ');
-            let name1 = val.next().unwrap().to_owned();
+            let name1 = hash(val.next().unwrap());
             let oper = Oper::from(val.next().unwrap());
-            let name2 = val.next().unwrap().to_owned();
+            let name2 = hash(val.next().unwrap());
 
             let monkey = Monkey {
                 first_monkey: name1,
@@ -89,14 +126,83 @@ fn solve_part1(s_monkeys: &mut HashMap<String, u64>, us_monkeys: &mut HashMap<St
         for key in keys_to_remove.drain(..) {
             us_monkeys.remove(&key.to_owned());
         }
-        if let Some(root) = s_monkeys.get("root") {
+        if let Some(root) = s_monkeys.get(&hash("root")) {
             println!("{root}");
             return;
         }
     }
 }
 
+fn solve_part2(s_monkeys: &mut HashMap<String, u64>, us_monkeys: &mut HashMap<String, Monkey>) {
+    let unsolvable = &find_humn(us_monkeys, "root").unwrap();
+    s_monkeys.remove(&"humn".to_owned());
+
+    let mut keys_to_remove = Vec::new();
+    let (first, second) = {
+        let root = us_monkeys.get("root").unwrap();
+        (root.first_monkey.to_owned(), root.second_monkey.to_owned())
+    };
+    loop {
+        for mnk_p in &*us_monkeys {
+            let mnk = &mnk_p.1;
+            if let Some((val1, val2)) = s_monkeys
+                .get(&mnk.first_monkey)
+                .zip(s_monkeys.get(&mnk.second_monkey))
+            {
+                let mnk_val = mnk.oper.do_oper(*val1, *val2);
+                s_monkeys.insert(mnk_p.0.to_owned(), mnk_val);
+                keys_to_remove.push(mnk_p.0.to_owned());
+            }
+        }
+        for key in keys_to_remove.drain(..) {
+            us_monkeys.remove(&key.to_owned());
+        }
+        //humn is not in us_monkeys
+        if us_monkeys.len() == (unsolvable.len() - 1) {
+            //need to reverse from the solved value in root
+            let mut val = *s_monkeys.get(&first).or(s_monkeys.get(&second)).unwrap();
+            for mnk in unsolvable
+                .iter()
+                .skip(1)
+                .rev()
+                .skip(1)
+                .map(|name| us_monkeys.get(name).unwrap())
+            {
+                //lhs is known
+                if let Some(mnk_val) = s_monkeys.get(&mnk.first_monkey) {
+                    val = mnk.oper.opposite().do_oper_right(val, mnk_val);
+                }
+                //rhs is known
+                else if let Some(mnk_val) = s_monkeys.get(&mnk.second_monkey) {
+                    val = mnk.oper.opposite().do_oper(val, mnk_val);
+                }
+            }
+
+            return;
+        }
+    }
+}
+
+fn find_humn(us_monkeys: &HashMap<String, Monkey>, name: &str) -> Option<Vec<String>> {
+    if name == "humn" {
+        return Some(vec![name.to_owned()]);
+    }
+
+    if let Some(mnk) = us_monkeys.get(&name.to_owned()) {
+        if let Some(mut vec) =
+            find_humn(us_monkeys, &mnk.first_monkey).or(find_humn(us_monkeys, &mnk.second_monkey))
+        {
+            vec.push(name.to_owned());
+            return Some(vec);
+        }
+    }
+
+    None
+}
+
 fn main() {
+    // let time = Instant::now();
     let (mut us_monkeys, mut s_monkeys) = parse_input(include_str!("input"));
     solve_part1(&mut s_monkeys, &mut us_monkeys);
+    // println!("{:?}", time.elapsed());
 }
