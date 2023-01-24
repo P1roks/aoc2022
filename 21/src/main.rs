bcb3976384fdef3e12902baed9edad9b5ddc42cd
P1roks <piotrekjakobczyk1@gmail.com>
2023-01-24 13:17:36 +0100

    day 21 hashing
    it cut down exec times in half

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -61,24 +61,23 @@ impl Oper {
 
 #[derive(Debug, Clone)]
 struct Monkey {
-    first_monkey: String,
-    second_monkey: String,
+    first_monkey: u64,
+    second_monkey: u64,
     oper: Oper,
 }
 
 //djb2, bcs imagine using Strings as keys
-fn hash(val: &str) -> String {
-    /* let mut hash: u64 = 5381;
+fn hash(val: &str) -> u64 {
+    let mut hash: u64 = 5381;
 
     for c in val.as_bytes() {
         hash = ((hash << 5) + hash) + *c as u64;
-    } */
+    }
 
-    // hash
-    val.to_owned()
+    hash
 }
 
-fn parse_input(file: &str) -> (HashMap<String, Monkey>, HashMap<String, u64>) {
+fn parse_input(file: &str) -> (HashMap<u64, Monkey>, HashMap<u64, u64>) {
     //solved and unsolved monkeys
     let mut s_monkeys = HashMap::new();
     let mut us_monkeys = HashMap::new();
@@ -109,7 +108,7 @@ fn parse_input(file: &str) -> (HashMap<String, Monkey>, HashMap<String, u64>) {
     (s_monkeys, us_monkeys)
 }
 
-fn solve_part1(s_monkeys: &mut HashMap<String, u64>, us_monkeys: &mut HashMap<String, Monkey>) {
+fn solve_part1(s_monkeys: &mut HashMap<u64, u64>, us_monkeys: &mut HashMap<u64, Monkey>) {
     let mut keys_to_remove = Vec::new();
     loop {
         for mnk_p in &*us_monkeys {
@@ -127,19 +126,19 @@ fn solve_part1(s_monkeys: &mut HashMap<String, u64>, us_monkeys: &mut HashMap<St
             us_monkeys.remove(&key.to_owned());
         }
         if let Some(root) = s_monkeys.get(&hash("root")) {
-            println!("{root}");
+            println!("result = {root}");
             return;
         }
     }
 }
 
-fn solve_part2(s_monkeys: &mut HashMap<String, u64>, us_monkeys: &mut HashMap<String, Monkey>) {
-    let unsolvable = &find_humn(us_monkeys, "root").unwrap();
-    s_monkeys.remove(&"humn".to_owned());
+fn solve_part2(s_monkeys: &mut HashMap<u64, u64>, us_monkeys: &mut HashMap<u64, Monkey>) {
+    let unsolvable = &find_humn(us_monkeys, hash("root")).unwrap();
+    s_monkeys.remove(&hash("humn"));
 
     let mut keys_to_remove = Vec::new();
     let (first, second) = {
-        let root = us_monkeys.get("root").unwrap();
+        let root = us_monkeys.get(&hash("root")).unwrap();
         (root.first_monkey.to_owned(), root.second_monkey.to_owned())
     };
     loop {
@@ -177,22 +176,22 @@ fn solve_part2(s_monkeys: &mut HashMap<String, u64>, us_monkeys: &mut HashMap<St
                     val = mnk.oper.opposite().do_oper(val, mnk_val);
                 }
             }
-
+            println!("result = {val}");
             return;
         }
     }
 }
 
-fn find_humn(us_monkeys: &HashMap<String, Monkey>, name: &str) -> Option<Vec<String>> {
-    if name == "humn" {
-        return Some(vec![name.to_owned()]);
+fn find_humn(us_monkeys: &HashMap<u64, Monkey>, name: u64) -> Option<Vec<u64>> {
+    if name == hash("humn") {
+        return Some(vec![name]);
     }
 
-    if let Some(mnk) = us_monkeys.get(&name.to_owned()) {
+    if let Some(mnk) = us_monkeys.get(&name) {
         if let Some(mut vec) =
-            find_humn(us_monkeys, &mnk.first_monkey).or(find_humn(us_monkeys, &mnk.second_monkey))
+            find_humn(us_monkeys, mnk.first_monkey).or(find_humn(us_monkeys, mnk.second_monkey))
         {
-            vec.push(name.to_owned());
+            vec.push(name);
             return Some(vec);
         }
     }
@@ -201,8 +200,8 @@ fn find_humn(us_monkeys: &HashMap<String, Monkey>, name: &str) -> Option<Vec<Str
 }
 
 fn main() {
-    // let time = Instant::now();
+    let time = Instant::now();
     let (mut us_monkeys, mut s_monkeys) = parse_input(include_str!("input"));
-    solve_part1(&mut s_monkeys, &mut us_monkeys);
-    // println!("{:?}", time.elapsed());
+    solve_part2(&mut s_monkeys, &mut us_monkeys);
+    println!("{:?}", time.elapsed());
 }
